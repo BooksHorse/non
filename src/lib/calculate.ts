48b95f69f10328133b03530ba952317a5e856132
mochi <57313924+BooksHorse@users.noreply.github.com>
2023-01-22 14:27:51 +0700

    v1

---
+++ src/lib/calculate.ts
@@ -0,0 +1,51 @@
+export function fareCalculate(kilometers: number, minutes: number) {
+    if (minutes % 2 == 0) {
+        minutes += 1;
+    }
+    let price = 0;
+    const price2 = minutes * 2;
+    if (kilometers > 80) {
+        const x_kilo = (kilometers - 80);
+        price += x_kilo * 10.50
+        kilometers -= x_kilo;
+    }
+    if (kilometers > 60) {
+        const x_kilo = (kilometers - 60)
+        price += x_kilo * 9
+        kilometers -= x_kilo
+    }
+    if (kilometers > 40) {
+        const x_kilo = (kilometers - 40)
+        price += x_kilo * 8
+        kilometers -= x_kilo
+    }
+    if (kilometers > 20) {
+        const x_kilo = (kilometers - 20)
+        price += x_kilo * 7.50
+        kilometers -= x_kilo
+    }
+    if (kilometers > 10) {
+        const x_kilo = (kilometers - 10)
+        price += x_kilo * 6.50
+        kilometers -= x_kilo
+    }
+    if (kilometers > 1) {
+        const x_kilo = (kilometers - 1)
+        price += x_kilo * 5.50
+        kilometers -= x_kilo
+    }
+    if (kilometers >= 1) {
+        price += 35
+    }
+    if (kilometers < 1) {
+        price += 35
+    }
+
+
+    if (price % 2 == 0) {
+        price += 1;
+    }
+
+    const total = price + price2
+    return total;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { fareCalculate } from '../lib/calculate';
+	import 'carbon-components-svelte/css/g100.css';
+	import { NumberInput, Tile, Content, Grid, Row, Column } from 'carbon-components-svelte';
+	let kilometers = 0;
+	let minutes = 0;
+</script>
+
+<svelte:head>
+	<title>Taxi Fare Calculator</title>
+</svelte:head>
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<h1>คำนวณระยะทาง Taxi</h1>
+				<p>
+					นำข้อมูลทั้งหมดมาจาก <a href="https://maps.google.com">Google Maps</a> หลังจากนั้นใส่ข้อมูลข้างล่าง
+				</p>
+			</Column>
+		</Row>
+		<Row>
+			<Column>
+				<!-- <p>กิโลเมตร <input bind:value={kilometers}></p>
+            <p>นาที <input bind:value={minutes}></p> -->
+				<NumberInput label="ระยะทาง (กิโลเมตร)" bind:value={kilometers} placeholder="ระยะทาง" />
+			</Column>
+		</Row>
+		<Row>
+			<Column>
+				<NumberInput
+					label="เวลาเวลาที่ใช้ในการเดินทาง (นาที)"
+					bind:value={minutes}
+					placeholder="เวลาที่ใช้ในการเดินทาง"
+				/>
+			</Column>
+		</Row>
+		<Row>
+			<Column>
+				<Tile>
+					<h2>
+						ค่า Taxi ประมาณ {fareCalculate(kilometers, minutes)} บาท
+					</h2>
+					<p>ค่าประมาณคลาดเคลื่อน 3-4 บาท</p>
+				</Tile>
+			</Column>
+		</Row>
+	</Grid>
+</Content>
