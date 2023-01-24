4c66303e42715bc145bc402bd43fe183af96304c
BooksHorse <57313924+BooksHorse@users.noreply.github.com>
2023-01-24 03:47:46 +0000

    fix no-empty

--- src/lib/calculate.ts
+++ src/lib/calculate.ts
@@ -1,27 +1,22 @@
 export function fareCalculate(kilometers: number, minutes: number) {
 	let total = 35;
 	if (kilometers <= 1) {
-	}
-	else if ( kilometers <= 10) {
-		total += kilometers*5.50;
-	}
-	else if (kilometers <= 20 ) {
-		total+= kilometers*6.50;
-	}
-	else if (kilometers <= 40) {
-		total+= kilometers*7.50;
-	}
-	else if (kilometers <= 60) {
-		total+= kilometers*8;
-	}
-	else if (kilometers <= 80) {
-		total+= kilometers*9;
-	}
-	else {
-		total+= kilometers*10.50
+		// do nothing
+	} else if (kilometers <= 10) {
+		total += kilometers * 5.5;
+	} else if (kilometers <= 20) {
+		total += kilometers * 6.5;
+	} else if (kilometers <= 40) {
+		total += kilometers * 7.5;
+	} else if (kilometers <= 60) {
+		total += kilometers * 8;
+	} else if (kilometers <= 80) {
+		total += kilometers * 9;
+	} else {
+		total += kilometers * 10.5;
 	}
 
-total+= minutes*1.50;
+	total += minutes * 1.5;
 
 	return total;
 }
